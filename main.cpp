2b75f030956fad3f72ddfaff058add7e5418d122
bstt <bstt.ue4@gmail.com>
2024-06-03 16:38:49 +0200

    iteration example

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "./bsttJson.hpp"
 
+#include <string>
+#include <vector>
+
 #include <iostream>
 
 struct Person
@@ -7,37 +10,43 @@ struct Person
 	std::string name;
 	int age;
 	bool isStudent;
-	double score;
+	std::vector<double> scoreList;
 	Person* friend_;
 };
 
 template <> inline Json toJson<Person>(const Person& person)
 {
-	Json json = JsonObj{{"name", person.name}, {"age", person.age}, {"isStudent", person.isStudent}, {"score", person.score}};
+	Json json
+		= JsonObj{{"name", person.name}, {"age", person.age}, {"isStudent", person.isStudent}, {"scoreList", person.scoreList}};
 	if (person.friend_) json["friend"] = *person.friend_;
 	return json;
 }
 template <> inline Person fromJson<Person>(const Json& json)
 {
-	Person person{json["name"], json["age"], json["isStudent"], json["score"], nullptr};
+	Person person{json["name"], json["age"], json["isStudent"], json["scoreList"], nullptr};
 	// you can also use the method get (cf. below) for type checking
-	// json.get("name", person.name, "age", person.age, "isStudent", person.isStudent, "score", person.score);
+	// json.get("name", person.name, "age", person.age, "isStudent", person.isStudent, "scoreList", person.scoreList);
 	json.tryGet("friend", *person.friend_);
 	return person;
 }
 
 int main()
 {
-	Person bstt{"bstt", 20, true, 100.0, nullptr};
+	Person bstt{"bstt", 20, true, {100.0, 95.3, 98.7}, nullptr};
 
-	Json json2 = Json::parse(R"({"age": 18,"isStudent": true,"name": "rui","score": 99.3})");
-	Person rui = json2;
+	Json json2 = Json::parse(R"({"age": 18,"isStudent": true,"name": "rui","scoreList": [99.3, 97.5, 96.8]})");
+	// browse scoreList without converting to Person
+	const JsonArr& scoreList = json2["scoreList"];
+	for (const Json& score : scoreList) std::cout << score << std::endl;
+	// to browse key-value pairs, use JsonObj (i.e. const JsonObj& obj = json2;)
+
+	Person rui = json2; // convert json to Person
 
 	rui.friend_ = &bstt;
 
-	Json json = rui;
+	Json json = rui; // convert Person to json
 
-	std::cout << json.toString("    ", "\n") << std::endl;
+	std::cout << json.toString("    ", "\n") << std::endl; // use toString function to format json
 
 	return 0;
 }
